222489c6655d470c70ac611ed7f9d15edf254c3c
bohdanruzek <162893308+bohdanruzek@users.noreply.github.com>
2025-07-12 21:42:55 +0200

    index.js

--- index.js
+++ index.js
@@ -44,11 +44,7 @@ const manifest = {
   resources: ['subtitles'],
   types: ['movie', 'series'],
   idPrefixes: ['tt'],
-  catalogs: [],
-  behaviorHints: {
-    configurable: false,
-    configurationRequired: false
-  }
+  catalogs: []
 };
 
 // Funkce pro čištění názvu filmu pro vyhledávání
@@ -325,12 +321,6 @@ app.get('/', (req, res) => {
 app.get('/manifest.json', (req, res) => {
   console.log('📋 Manifest požadavek');
   res.json(manifest);
-});
-
-// Wildcard debug endpoint - PŘED normální endpoint
-app.get('/subtitles/*', (req, res) => {
-  console.log(`🔥 WILDCARD DEBUG: ${req.url}`);
-  res.json({ subtitles: [] });
 });
 
 // Subtitles endpoint s debug loggingem
